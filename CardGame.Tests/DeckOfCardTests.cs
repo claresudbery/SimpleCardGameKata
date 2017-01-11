56c969e7e61220408b284650f7f02dfd88af4fac
claresudbery <clareworking@claresudbery.co.uk>
2017-01-11 23:54:05 +0000

    Refactor: Use Enumerable.Range and put expected values in correct order.

--- CardGame.Tests/DeckOfCardTests.cs
+++ CardGame.Tests/DeckOfCardTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleCardGameKata;
 
@@ -11,7 +12,7 @@ namespace CardGame.Tests
         public void ShuffleChangesTheOrderOfTheCards()
         {
             // Arrange
-            var cardsBeforeShuffle = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var cardsBeforeShuffle = Enumerable.Range(1, 10).ToList();
             DeckOfCards deckOfCards = new DeckOfCards(cardsBeforeShuffle);
 
             // Act
@@ -25,23 +26,23 @@ namespace CardGame.Tests
         public void CardsAreDealtInAlternateOrder()
         {
             // Arrange
-            DeckOfCards deckOfCards = new DeckOfCards(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            DeckOfCards deckOfCards = new DeckOfCards(Enumerable.Range(1, 10));
 
             // Act
             List<List<int>> dealtCards = deckOfCards.Deal(2);
 
             // Assert
-            Assert.AreEqual(dealtCards[0][0], 1);
-            Assert.AreEqual(dealtCards[0][1], 3);
-            Assert.AreEqual(dealtCards[0][2], 5);
-            Assert.AreEqual(dealtCards[0][3], 7);
-            Assert.AreEqual(dealtCards[0][4], 9);
+            Assert.AreEqual(1, dealtCards[0][0]);
+            Assert.AreEqual(3, dealtCards[0][1]);
+            Assert.AreEqual(5, dealtCards[0][2]);
+            Assert.AreEqual(7, dealtCards[0][3]);
+            Assert.AreEqual(9, dealtCards[0][4]);
 
-            Assert.AreEqual(dealtCards[1][0], 2);
-            Assert.AreEqual(dealtCards[1][1], 4);
-            Assert.AreEqual(dealtCards[1][2], 6);
-            Assert.AreEqual(dealtCards[1][3], 8);
-            Assert.AreEqual(dealtCards[1][4], 10);
+            Assert.AreEqual(2, dealtCards[1][0]);
+            Assert.AreEqual(4, dealtCards[1][1]);
+            Assert.AreEqual(6, dealtCards[1][2]);
+            Assert.AreEqual(8, dealtCards[1][3]);
+            Assert.AreEqual(10, dealtCards[1][4]);
         }
     }
 }
