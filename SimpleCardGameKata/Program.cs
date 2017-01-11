a3075da486dd568db892e5982f1df81377f1a0fa
claresudbery <clareworking@claresudbery.co.uk>
2017-01-11 23:54:19 +0000

    Better output formatting

--- SimpleCardGameKata/Program.cs
+++ SimpleCardGameKata/Program.cs
@@ -13,15 +13,26 @@ namespace SimpleCardGameKata
         {
             int numPlayers = 1;
             int numHands = numPlayers + 1;
-            var deckOfCards = new DeckOfCards(new List<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            int numCards = numHands * 5;
+            var deckOfCards = new DeckOfCards(Enumerable.Range(1, numCards));
             Console.WriteLine("Hello and welcome!");
+            Console.WriteLine("");
             Console.WriteLine("Here is the start deck: " + CardsAsString(deckOfCards.Cards));
 
             deckOfCards.Shuffle();
             Console.WriteLine("Here is the shuffled deck: " + CardsAsString(deckOfCards.Cards));
+            Console.WriteLine("");
 
-            List<int> playerCards = deckOfCards.Deal(numHands)[1];
-            Console.WriteLine("This is a one-player game. You are the player. Here are the cards you have been dealt: " + CardsAsString(playerCards));
+            List<List<int>> allCards = deckOfCards.Deal(numHands);
+            List<int> playerCards = allCards[1];
+            Console.WriteLine("This is a one-player game. You are the player.");
+            Console.WriteLine("Here are the cards you have been dealt: " + CardsAsString(playerCards));
+            Console.WriteLine("");
+
+            Game game = new Game(allCards);
+            int kittyCard = game.PlayKittyCard();
+            Console.WriteLine("First kitty card played: " + kittyCard);
+            Console.WriteLine("");
 
             Console.WriteLine("Press enter to exit.");
             Console.ReadLine();
